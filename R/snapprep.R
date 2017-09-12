1dcb5cf5f4addda762e329fd3dc04241c2e95ca5
leonawicz <matt.rstat@gmail.com>
2017-09-12 17:33:39 -0600

    Update defaults.

--- R/snapprep.R
+++ R/snapprep.R
@@ -29,23 +29,27 @@ NULL
 #' @examples
 #' snapdef()$ar5gcm
 snapdef <- function(){
+  bsalf <- "/big_scratch/mfleonawicz/Alf_Files_20121129"
+  datex <- "/atlas_scratch/mfleonawicz/projects/DataExtraction/workspaces"
+  alfex <- "/atlas_scratch/mfleonawicz/alfresco"
+  mldat <- "/workspace/UA/mfleonawicz/data"
   list(
     ar5dir = "/workspace/Shared/Tech_Projects/DeltaDownscaling/project_data/downscaled", # nolint start
-    ar5dir_dist_monthly = "/workspace/UA/mfleonawicz/data/clim_2km_monthly",
-    ar5dir_dist_seasonal = "/workspace/UA/mfleonawicz/data/clim_2km_seasonal",
-    ar5dir_dist_stats = paste0("/workspace/UA/mfleonawicz/data/clim_2km_", c("monthly", "seasonal"), "_stats"),
+    ar5dir_dist_monthly = file.path(mdat, "clim_2km_monthly"),
+    ar5dir_dist_seasonal = file.path(mldat, "clim_2km_seasonal"),
+    ar5dir_dist_stats = paste0(mldat, "/clim_2km_", c("monthly", "seasonal"), "_stats"),
     ar5gcm = c("GFDL-CM3", "GISS-E2-R", "IPSL-CM5A-LR", "MRI-CGCM3", "NCAR-CCSM4"),
     ar5cru = "ts40",
     ar5all = c("GFDL-CM3", "GISS-E2-R", "IPSL-CM5A-LR", "MRI-CGCM3", "NCAR-CCSM4", "ts40"),
     ar5var = c("pr", "tas", "tasmin", "tasmax"),
     template_akcan1km = "/atlas_scratch/apbennett/IEM/FinalCalib/cccma_cgcm3_1.sresa1b/Maps/1900/Age_0_1900.tif",
     template_akcan2km = "/Data/Base_Data/Climate/AK_CAN_2km/projected/AR5_CMIP5_models_deprecated/rcp60/5ModelAvg/pr/pr_total_mm_AR5_5ModelAvg_rcp60_01_2006.tif",
-    template_ak1km = "/atlas_scratch/mfleonawicz/alfresco/CMIP5_Statewide/outputs/3m/rcp45.CCSM4/Maps/2014/Age_0_2014.tif",
-    veg_ak = "/big_scratch/mfleonawicz/Alf_Files_20121129/alf2005.cavm.merged.030212.tif",
-    fmoBuffer_ak = "/big_scratch/mfleonawicz/Alf_Files_20121129/fmo_2017_buffered.tif",
-    fmoRatios_ak = "/big_scratch/mfleonawicz/Alf_Files_20121129/fmo_2017_buffered_fs.tif",
-    cells_akcan1km2km = "/atlas_scratch/mfleonawicz/projects/DataExtraction/workspaces/cells_akcan1km2km.rds",
-    cells_ak1km = "/atlas_scratch/mfleonawicz/projects/DataExtraction/workspaces/cells_ak1km.rds",
-    celldir = "/workspace/UA/mfleonawicz/projects/DataExtraction/workspaces" # nolint end
+    template_ak1km = file.path(alfex, "CMIP5_Statewide/outputs/3m/rcp45.CCSM4/Maps/2014/Age_0_2014.tif"),
+    veg_ak = file.path(bsalf, "alf2005.cavm.merged.030212.tif"),
+    fmoBuffer_ak = file.path(bsalf, "fmo_2017_buffered.tif"),
+    fmoRatios_ak = file.path(bsalf, "fmo_2017_buffered_fs.tif"),
+    cells_akcan1km2km = file.path(datex, "cells_akcan1km2km.rds"),
+    cells_ak1km = file.path(datex, "cells_ak1km.rds"),
+    celldir = datex # nolint end
   )
 }
