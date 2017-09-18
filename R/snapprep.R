55a7c394ae3118e72ba072110e500d947be31061
leonawicz <matt.rstat@gmail.com>
2017-09-18 16:39:22 -0600

    Add climate directory.

--- R/snapprep.R
+++ R/snapprep.R
@@ -36,9 +36,9 @@ snapdef <- function(){
   list(
     dist_dir = mldat,
     ar5dir = "/workspace/Shared/Tech_Projects/DeltaDownscaling/project_data/downscaled",
-    ar5dir_dist_monthly = file.path(mldat, "clim_2km_monthly"),
-    ar5dir_dist_seasonal = file.path(mldat, "clim_2km_seasonal"),
-    ar5dir_dist_stats = paste0(mldat, "/clim_2km_", c("monthly", "seasonal"), "_stats"),
+    ar5dir_dist_monthly = file.path(mldat, "climate/clim_2km_monthly"),
+    ar5dir_dist_seasonal = file.path(mldat, "climate/clim_2km_seasonal"),
+    ar5dir_dist_stats = paste0(mldat, "/climate/clim_2km_", c("monthly", "seasonal"), "_stats"),
     ar5gcm = c("GFDL-CM3", "GISS-E2-R", "IPSL-CM5A-LR", "MRI-CGCM3", "NCAR-CCSM4"),
     ar5cru = "ts40",
     ar5all = c("GFDL-CM3", "GISS-E2-R", "IPSL-CM5A-LR", "MRI-CGCM3", "NCAR-CCSM4", "ts40"),
