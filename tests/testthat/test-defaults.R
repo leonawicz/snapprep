803a3c0fb382eca7e9971089454a09d1a054803c
leonawicz <matt.rstat@gmail.com>
2017-09-22 16:01:14 -0600

    Fix test.

--- tests/testthat/test-defaults.R
+++ tests/testthat/test-defaults.R
@@ -1,11 +1,10 @@
 context("defaults")
 
-x < - snapdef()
+x <- snapdef()
 
 test_that("snap defaults are as expected", {
   expect_is(x, "list")
   expect_equal(length(x$ar5dir_dist_stats), 2)
   expect_equal(x$ar5cru, x$ar5all[6])
-  purrr::walk(xlist, ~expect_identical(get_levels(.x), lev))
   expect_equal(length(x), 18)
 })
